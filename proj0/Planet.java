7fab0aab66acc4a4a2986dae3f4fbc1555f05a3f
Klayang <68698886+Klayang@users.noreply.github.com>
2022-08-02 17:32:34 +0800

    Update Planet.java
    change public to private for G in planet class

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
         yyPos = yP;
