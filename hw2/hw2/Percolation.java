bd6162dc1943e8ccdf2045339a80c1a459881e8f
Klayang <68698886+Klayang@users.noreply.github.com>
2022-09-15 15:48:18 +0800

    Add files via upload

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,87 +1,135 @@
 package hw2;
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation 
-{
-    private boolean [][] open;
-    private final int n;
-    private int NumberOfOpenSites = 0;
-    private final WeightedQuickUnionUF wquf;
-    private final WeightedQuickUnionUF WQUF;
-    public Percolation(int n)
-    {
-        if(n <= 0) throw new IllegalArgumentException();
-        wquf = new WeightedQuickUnionUF(n*n + 2);
-        WQUF = new WeightedQuickUnionUF(n*n + 1);
-        this.n = n;
-        open = new boolean[n+2][n+2];
-        for(int i = 0; i <= n + 1; i++)
-            for(int j = 0; j <= n + 1; j++)
-                open[i][j] = false;
+public class Percolation {
+    private boolean[][] open;
+    private int N;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufToAvoidBackwash;
+    private int topSite;
+    private int bottomSite;
+    private int numOfOpenSites;
+    /**
+     * create N-by-N grid, with all sites initially blocked
+     * @param N width & height of the grids
+     */
+    public Percolation(int N) {
+        // exception handle
+        if (N <= 0) throw new IllegalArgumentException();
+
+        // Initialize instances
+        this.N = N;
+        open = new boolean[N][N];
+        uf = new WeightedQuickUnionUF(N * N + 2);
+        ufToAvoidBackwash = new WeightedQuickUnionUF(N * N + 1);
+        topSite = N * N;
+        bottomSite = topSite + 1;
+        numOfOpenSites = 0;
     }
-    public void open(int row, int col)
-    {
-        exception(row, col);
-        if(isOpen(row, col)) return;
+
+    /**
+     * open the site (row, col) if it is not open already
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        // exception handle
+        if (row < 0 || row >= N || col < 0 || col >= N) throw new IndexOutOfBoundsException();
+        // open the prescribed site
         open[row][col] = true;
-        if(row == 1 && n == 1) 
-        {
-            wquf.union(1, 0);
-            wquf.union(1, 2);
-            WQUF.union(1, 0);
+        connect(row, col);
+        numOfOpenSites++;
+    }
+
+    /**
+     * is the site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        // exception handle
+        if (row < 0 || row >= N || col < 0 || col >= N) throw new IndexOutOfBoundsException();
+        // check whether the site is open
+        return open[row][col];
+    }
+
+    /**
+     * is the site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        // exception handle
+        if (row < 0 || row >= N || col < 0 || col >= N) throw new IndexOutOfBoundsException();
+        // check whether the site is full
+        return ufToAvoidBackwash.connected(index(row, col), topSite);
+    }
+
+    /**
+     * number of open sites
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return numOfOpenSites;
+    }
+
+    /**
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return uf.connected(topSite, bottomSite);
+    }
+
+    /**
+     * Transform index of a 2d array to index of its 1d version
+     * @param i, row index
+     * @param j, column index
+     * @return index of 1d version
+     */
+    private int index(int i, int j) {
+        return i * N + j;
+    }
+
+    /**
+     * try connecting the current grip with grids beside it
+     * @param i, row index of the grid
+     * @param j, column index of the grid
+     */
+    private void connect(int i, int j) {
+        if (j > 0 && open[i][j - 1]) {
+            uf.union(index(i, j), index(i, j - 1));
+            ufToAvoidBackwash.union(index(i, j), index(i, j - 1));
         }
-        if(row == 1) 
-        {
-            wquf.union(col, 0);
-            if(open[row + 1][col]) wquf.union(col, n + col);
-            WQUF.union(col, 0);
-            if(open[row + 1][col]) WQUF.union(col, n + col);
+        if (j < N - 1 && open[i][j + 1]) {
+            uf.union(index(i, j), index(i, j + 1));
+            ufToAvoidBackwash.union(index(i, j), index(i, j + 1));
         }
-        else if(row == n) 
-        {
-            wquf.union(n*row + col - n, n*n + 1);
-            if(open[row - 1][col]) wquf.union(n*row + col - n, n*row + col - 2*n);
-            if(open[row - 1][col]) WQUF.union(n*row + col - n, n*row + col - 2*n);
+        if (i > 0 && open[i - 1][j]) {
+            uf.union(index(i, j), index(i - 1, j));
+            ufToAvoidBackwash.union(index(i, j), index(i - 1, j));
         }
-        else
-        {
-            if(open[row - 1][col]) wquf.union(n*row + col - n, n*row + col - 2*n);
-            if(open[row + 1][col]) wquf.union(n*row + col - n, n*row + col);
-            if(open[row - 1][col]) WQUF.union(n*row + col - n, n*row + col - 2*n);
-            if(open[row + 1][col]) WQUF.union(n*row + col - n, n*row + col);
+        if (i < N - 1 && open[i + 1][j]) {
+            uf.union(index(i, j), index(i + 1, j));
+            ufToAvoidBackwash.union(index(i, j), index(i + 1, j));
         }
-        if(open[row][col - 1]) wquf.union(n*row + col - n, n*row + col - n - 1);
-        if(open[row][col + 1]) wquf.union(n*row + col - n, n*row + col - n + 1);
-        if(open[row][col - 1]) WQUF.union(n*row + col - n, n*row + col - n - 1);
-        if(open[row][col + 1]) WQUF.union(n*row + col - n, n*row + col - n + 1);
-        NumberOfOpenSites++;
+        if (i == 0) {
+            uf.union(index(i, j), topSite);
+            ufToAvoidBackwash.union(index(i, j), topSite);
+        }
+        if (i == N - 1) uf.union(index(i, j), bottomSite);
     }
-    public boolean isOpen(int row, int col)
-    {
-        exception(row, col);
-        return open[row][col] == true;
-    }
-    public int numberOfOpenSites()
-    {
-        return NumberOfOpenSites;
-    }
-    public boolean isFull(int row, int col)
-    {
-        exception(row, col);
-        return WQUF.find(0) == WQUF.find(n*row + col - n);
-    }
-    public boolean percolates()
-    {
-        return wquf.find(0) == wquf.find(n*n + 1);
-    }
-    private void exception(int row, int col)
-    {
-        if(row <= 0 || row >= n + 1 || col <= 0 || col >= n + 1) throw new IllegalArgumentException();
-    }
-    public static void main(String[] argv)
-    {
-        Percolation per = new Percolation(3);
-        per.open(1, 3);per.open(2, 3);per.open(3, 3);per.open(3, 1);
-        System.out.println(per.isFull(3, 1));
+
+    /**
+     * Unit test
+     * @param args
+     */
+    public static void main(String[] args) {
+        Percolation p = new Percolation(2);
+        p.open(0, 0); p.open(1, 1); p.open(0, 1);
+        System.out.println(p.percolates());
     }
 }
