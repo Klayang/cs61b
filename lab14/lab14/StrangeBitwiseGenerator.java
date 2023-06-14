4ef698777f422ee46cdffcba1a37756923a91be2
Klayang <68698886+Klayang@users.noreply.github.com>
2023-06-14 21:35:09 +0800

    Add files via upload

--- lab14/lab14/StrangeBitwiseGenerator.java
+++ lab14/lab14/StrangeBitwiseGenerator.java
@@ -12,6 +12,9 @@ public class StrangeBitwiseGenerator implements Generator {
     @Override
     public double next() {
         state += 1;
-        return state & (state >>> 3) % period;
+        return normalize(state & (state >>> 3) % period);
+    }
+    protected double normalize(int value) {
+        return (double)value / (double)period * 2 - 1.0;
     }
 }
